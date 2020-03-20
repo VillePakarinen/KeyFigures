6396c5ce019b0c0fc6c26dbb6d845c2f7a5c4f7d
Ville Pakarinen <ville.pakarinen@stat.fi>
2020-03-20 09:43:17 +0200

    Add expandable table row

--- src/components/keyFiguresTable/KeyFigureTable.tsx
+++ src/components/keyFiguresTable/KeyFigureTable.tsx
@@ -10,6 +10,7 @@ import Paper from "@material-ui/core/Paper";
 
 import { Municipality } from "../../model/municipalitiesDto";
 import { KeyFigure } from "../../model/keyFigureDto";
+import ExpandableRow from "./expandaleRow/ExpandingRow";
 
 interface Props {
   primaryMunicipality?: Municipality;
@@ -21,6 +22,18 @@ interface Props {
 const useStyles = makeStyles({
   table: {
     minWidth: 400
+  },
+  container: {
+    maxHeight: "70vh"
+  },
+  labelColumn: {
+    width: "60%"
+  },
+  valueColumn: {
+    width: "25%"
+  },
+  buttonColumn: {
+    width: "5%"
   }
 });
 
@@ -34,26 +47,37 @@ const KeyFigureTable: React.FC<Props> = ({
 
   return (
     <div>
-      KeyFigures: for {JSON.stringify(primaryMunicipality)} and
-      {JSON.stringify(secondaryMunicipality)}
       <TableContainer component={Paper}>
-        <Table className={classes.table}>
+        <Table className={classes.table} size="small" aria-label="Kuntien avainlukujen taulukko">
           <TableHead>
             <TableRow>
-              <TableCell>Avainluku, tiedot vuodelta </TableCell>
-              <TableCell>{primaryMunicipality?.name}</TableCell>
-              <TableCell>{secondaryMunicipality?.name}</TableCell>
+              <TableCell className={classes.labelColumn}>Avainluku, tiedot vuodelta </TableCell>
+              <TableCell className={classes.valueColumn}>{primaryMunicipality?.name}</TableCell>
+              <TableCell className={classes.valueColumn}>{secondaryMunicipality?.name}</TableCell>
+              <TableCell></TableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {primaryKeyFigures &&
               primaryKeyFigures.map((keyFigure, index) => {
                 return (
-                  <TableRow key={keyFigure.id}>
-                    <TableCell>{keyFigure.label}</TableCell>
-                    <TableCell>{keyFigure.value}</TableCell>
-                    <TableCell>{secondaryKeyFigures?.[index]?.value}</TableCell>
-                  </TableRow>
+                  <ExpandableRow
+                    key={keyFigure.id}
+                    expandable={true}
+                    values={[
+                      keyFigure.label,
+                      keyFigure.value.toString(),
+                      secondaryKeyFigures?.[index]?.value.toString()
+                    ]}
+                    render={
+                      <div>
+                        This should be visible
+                        <div>This should be visible</div>
+                        <div>This should be visible</div>
+                        <div>This should be visible</div>
+                      </div>
+                    }
+                  />
                 );
               })}
           </TableBody>
---
+++ src/components/keyFiguresTable/expandaleRow/ExpandingRow.tsx
@@ -0,0 +1,70 @@
+import React, { useState } from "react";
+import TableCell from "@material-ui/core/TableCell";
+import TableRow from "@material-ui/core/TableRow";
+import Collapse from "@material-ui/core/Collapse";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { IconButton, makeStyles } from "@material-ui/core";
+import transitions from "@material-ui/core/styles/transitions";
+
+interface Props {
+  values: string[];
+  expandable: boolean;
+  render: any;
+}
+
+const useStyles = makeStyles({
+  dropdown: {
+    transition: transitions.create(["transform"], {
+      duration: transitions.duration.short
+    })
+  },
+  dropdownOpen: {
+    transform: "rotate(180deg)"
+  },
+  dropdownClosed: {
+    transform: "rotate(0)"
+  }
+});
+
+const ExpandableRow: React.FC<Props> = ({ values, expandable, render }) => {
+  const [isOpen, setOpen] = useState(false);
+
+  const classes = useStyles();
+
+  const expandableIcon = expandable ? (
+    <TableCell>
+      <IconButton aria-label="Expand panel" onClick={() => setOpen(!isOpen)}>
+        <ExpandMoreIcon
+          className={[
+            classes.dropdown,
+            isOpen ? classes.dropdownOpen : classes.dropdownClosed
+          ].join(" ")}
+        />
+      </IconButton>
+    </TableCell>
+  ) : null;
+
+  const expandableContent = expandable ? (
+    <TableRow aria-hidden={!isOpen}>
+      <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={values.length + 1}>
+        <Collapse in={isOpen} unmountOnExit timeout="auto">
+          {render}
+        </Collapse>
+      </TableCell>
+    </TableRow>
+  ) : null;
+
+  return (
+    <>
+      <TableRow>
+        {values.map(value => (
+          <TableCell>{value}</TableCell>
+        ))}
+        {expandableIcon}
+      </TableRow>
+      {expandableContent}
+    </>
+  );
+};
+
+export default ExpandableRow;
